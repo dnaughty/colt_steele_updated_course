9cfdf5e02d60ffd8b09eac7a04685540426d11df
Denis Naughton <mjolinor75@hotmail.com>
2020-12-23 14:56:41 -0500

    finished else if

--- javascript/conditionals/conditional.js
+++ javascript/conditionals/conditional.js
@@ -1,7 +1,17 @@
 
-let random = Math.random();
-if (random < 0.5) {
-    console.log("Your number is less than 0.5")
-    console.log(random);
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("Your number is less than 0.5")
+//     console.log(random);
+// }
+
+const dayOfTheWeek = 'Boner'
+
+if (dayOfTheWeek == 'Monday') {
+    console.log("I hate Monday")
+} else if (dayOfTheWeek == 'Saturday') {
+    console.log("Yay!")
+} else {
+    console.log("Just another day")
 }
 
