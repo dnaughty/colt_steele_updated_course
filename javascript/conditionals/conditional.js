b41187af4e3771ac63f1fd7b246e76cfc50adda2
Denis Naughton <mjolinor75@hotmail.com>
2020-12-26 17:22:09 -0500

    finished nested

--- javascript/conditionals/conditional.js
+++ javascript/conditionals/conditional.js
@@ -5,14 +5,24 @@
 //     console.log(random);
 // }
 
-const dayOfTheWeek = prompt('Enter a Day')
+// const dayOfTheWeek = prompt('Enter a Day')
 
-if (dayOfTheWeek == 'Monday') {
-    console.log("I hate Monday")
-} else if (dayOfTheWeek == 'Saturday') {
-    console.log("Yay!")
-} else if (dayOfTheWeek == "Friday") {
-    console.log("great")
-} 
+// if (dayOfTheWeek == 'Monday') {
+//     console.log("I hate Monday")
+// } else if (dayOfTheWeek == 'Saturday') {
+//     console.log("Yay!")
+// } else if (dayOfTheWeek == "Friday") {
+//     console.log("great")
+// } 
 
+const password = prompt("Please enter a new password");
 
+if (password.length >= 6) {
+    if (password.indexOf(' ') !== -1) {
+        console.log("No spaces!");
+} else {
+    console.log("valid");
+}
+} else {
+    console.log("Too Short");
+}
