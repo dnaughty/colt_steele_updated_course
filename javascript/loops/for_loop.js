e169f4bc1071a40bc994e5c7841f7654d4844d39
Denis Naughton <mjolinor75@hotmail.com>
2021-01-08 14:59:27 -0500

    Decremented numbers

--- javascript/loops/for_loop.js
+++ javascript/loops/for_loop.js
@@ -12,6 +12,10 @@
     
 // }
 
-for (let i = 0; i <= 20; i += 2) {
+// for (let i = 2; i <= 20; i += 2) {
+//     console.log(i)
+// }
+
+for (let i = 100; i >= 0; i -= 10) {
     console.log(i)
 }
