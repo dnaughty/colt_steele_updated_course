18ca85b3516268cdde7b11ef6cc8a1a9812391f2
Denis Naughton <mjolinor75@hotmail.com>
2021-01-07 20:15:16 -0500

    Finished  first loop exercise

--- javascript/loops/for_loop.js
+++ javascript/loops/for_loop.js
@@ -1,3 +1,13 @@
-for(let i = 1; i <=10; i++){
-    console.log(i)
+// for(let i = 1; i <=10; i++){
+//     console.log("I'm in the loop")
+//     console.log(i);
+// }
+
+// console.log("Loop Over")
+
+// Print out "Da ba dee da ba daa" 6 times, using a for loop
+
+for(let i = 1; i<=6; i++){
+    console.log("Da ba dee da ba daa")
+    
 }
