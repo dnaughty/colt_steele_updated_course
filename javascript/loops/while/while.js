092d217b04b1e7bed4e46b823334e892f1fabc9b
Denis Naughton <mjolinor75@hotmail.com>
2021-01-09 17:53:24 -0500

    Created for with break

--- javascript/loops/while/while.js
+++ javascript/loops/while/while.js
@@ -14,13 +14,18 @@
 
 // console.log("enter")
 
-let input = prompt("Hey, say something")
+// let input = prompt("Hey, say something")
 
-while (true) {
-    input = prompt(input);
-    if (input === "stop") {
-        break;
-    }
+// while (true) {
+//     input = prompt(input);
+//     if (input === "stop") {
+//         break;
+//     }
+// }
+
+// console.log("you win")
+
+for (let i = 0; i < 1000; i++) {
+    console.log(i);
+    if(i === 100) break;
 }
-
-console.log("you win")
