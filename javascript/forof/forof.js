31d0d7464f45505aa447190d481fd9a008bf35cd
Denis Naughton <mjolinor75@hotmail.com>
2021-01-12 18:27:19 -0500

    Finished nested for loops

--- javascript/forof/forof.js
+++ javascript/forof/forof.js
@@ -1,5 +1,17 @@
-const subreddits = ['cringe', 'books', 'chickens', 'funny', 'pics', 'soccer']
+// const subreddits = ['cringe', 'books', 'chickens', 'funny', 'pics', 'soccer']
 
-for (let sub of subreddits) {
-    console.log(`Visit reddit.com/r/${sub}`)
+// for (let sub of subreddits) {
+//     console.log(`Visit reddit.com/r/${sub}`)
+// }
+
+const seatingChart = [
+    ['Kristen', 'Erik', 'Namita'],
+    ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+    ['Yuma', 'Sakura', 'Jack', 'Erika']
+]
+
+for (let row of seatingChart) {
+    for (let student of row) {
+        console.log(student);
+    }
 }
