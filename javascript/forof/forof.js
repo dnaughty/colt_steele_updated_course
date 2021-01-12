1e07a4f80f3010464845aa9e338b83d86f4778ae
Denis Naughton <mjolinor75@hotmail.com>
2021-01-12 18:31:34 -0500

    Finished iterating over a string

--- javascript/forof/forof.js
+++ javascript/forof/forof.js
@@ -4,14 +4,18 @@
 //     console.log(`Visit reddit.com/r/${sub}`)
 // }
 
-const seatingChart = [
-    ['Kristen', 'Erik', 'Namita'],
-    ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
-    ['Yuma', 'Sakura', 'Jack', 'Erika']
-]
+// const seatingChart = [
+//     ['Kristen', 'Erik', 'Namita'],
+//     ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+//     ['Yuma', 'Sakura', 'Jack', 'Erika']
+// ]
 
-for (let row of seatingChart) {
-    for (let student of row) {
-        console.log(student);
-    }
+// for (let row of seatingChart) {
+//     for (let student of row) {
+//         console.log(student);
+//     }
+// }
+
+for (let char of "Hello world") {
+    console.log(char)
 }
