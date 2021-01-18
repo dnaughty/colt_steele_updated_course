489b1aedbef067feb314512c8ed608fceb56dc2a
Denis Naughton <mjolinor75@hotmail.com>
2021-01-18 18:59:30 -0500

    created object

---
+++ javascript/js_objects/object.js
@@ -0,0 +1,12 @@
+const testScores = {
+    keenan: 80,
+    damon: 67,
+    kim: 89,
+    shawn: 91,
+    marlon: 72,
+    dwayne: 77,
+    nadia: 83,
+    elvira: 97,
+    diedre: 81,
+    vonnie: 60
+}
