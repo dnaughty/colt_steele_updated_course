698533ffd92cfcf926f97981bcd485a36f228923
Denis Naughton <mjolinor75@hotmail.com>
2021-01-21 13:32:14 -0500

    My attempt to create todo

--- javascript/todo_list/todo.js
+++ javascript/todo_list/todo.js
@@ -1,1 +1,15 @@
-console.log("Hello")
+
+const list = []
+
+let item = prompt("Add an item")
+
+
+if(item === "end") {
+    break;
+} else {
+    let item = prompt("Add an item");
+    list.push(item)
+}
+
+
+console.log(list)
