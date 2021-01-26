51742c2242d1cc3c7f119ff10441171dc9929b31
Denis Naughton <mjolinor75@hotmail.com>
2021-01-25 19:59:24 -0500

    finished higher order functions

--- javascript/functions/scope/scope.js
+++ javascript/functions/scope/scope.js
@@ -3,6 +3,25 @@
 //     console.log(totalEggs);
 // }
 
-const add = function(x,y) {
-    return x + y;
+// const add = function(x,y) {
+//     return x + y;
+// }
+
+// let greet = function() { console.log("HI")}
+
+function callTwice(func) {
+    func();
+    func();
+
+}
+
+function rollDie(){
+    const roll = Math.floor(Math.random() * 6) + 1
+    console.log(roll)
+}
+
+function callTenTimes(f) {
+    for (let i = 0; i < 10; i++) {
+        f()
+    }
 }
