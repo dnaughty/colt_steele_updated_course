12cefb82978f26e12f86b666a00de927ef760b27
Denis Naughton <mjolinor75@hotmail.com>
2021-01-26 19:26:01 -0500

    finished mystery func

--- javascript/functions/scope/scope.js
+++ javascript/functions/scope/scope.js
@@ -9,19 +9,35 @@
 
 // let greet = function() { console.log("HI")}
 
-function callTwice(func) {
-    func();
-    func();
+// function callTwice(func) {
+//     func();
+//     func();
 
-}
+// }
 
-function rollDie(){
-    const roll = Math.floor(Math.random() * 6) + 1
-    console.log(roll)
-}
+// function rollDie(){
+//     const roll = Math.floor(Math.random() * 6) + 1
+//     console.log(roll)
+// }
 
-function callTenTimes(f) {
-    for (let i = 0; i < 10; i++) {
-        f()
+// function callTenTimes(f) {
+//     for (let i = 0; i < 10; i++) {
+//         f()
+//     }
+// }
+
+function makeMysteryFunc(num) {
+    // const rand = Math.random();
+    if(num > 10){
+        return function() {
+            console.log("Congrats, I'm a good function!")
+        }
+    } else {
+        return function() {
+            alert("Virus!")
+            alert("Don't close")
+            alert("Don't close")
+            alert("Don't close")
+        }
     }
 }
