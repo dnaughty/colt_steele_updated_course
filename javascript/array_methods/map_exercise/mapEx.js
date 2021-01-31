f9a679c0a79ec1df62aab6b2c09a6eea7e39ae16
Denis Naughton <mjolinor75@hotmail.com>
2021-01-31 18:25:04 -0500

    created arrow_methods

--- javascript/array_methods/map_exercise/mapEx.js
+++ javascript/array_methods/map_exercise/mapEx.js
@@ -1,5 +1,15 @@
 const names = ["  Timothee", "Darth_hater   ", "elton John    "] 
 
-const cleanNames = names.map(function(name){
-    return name.trim()
+// const cleanNames = names.map(function(name){
+//     return name.trim()
+// })
+
+function cleanNames(array) {
+    array.map{
+        return n.trim()
+    }
+}
+
+const ratings = movies.map(function(rate){
+    return rate.score
 })
