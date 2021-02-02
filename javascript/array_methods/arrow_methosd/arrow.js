830e4b543c43391b72d0b9eac80ccf041db98b42
Denis Naughton <mjolinor75@hotmail.com>
2021-02-02 16:17:37 -0500

    finished arrow implicit return

--- javascript/array_methods/arrow_methosd/arrow.js
+++ javascript/array_methods/arrow_methosd/arrow.js
@@ -28,14 +28,24 @@ const titles = movies.map(function(movie){
 })
 
 
-const add = (x,y) => {
-    return x + y
-}
+// const add = (x,y) => {
+//     return x + y
+// }
 
 const square = (x) => {
     return x * x;
 }
 
-const rollDie = () => {
-    return Math.floor(Math.random() * 6 ) + 1
-}
+// const rollDie = () => {
+//     return Math.floor(Math.random() * 6 ) + 1
+// }
+
+// const rollDie = () => (
+//      Math.floor(Math.random() * 6 ) + 1
+// )
+
+const add = (x,y) => x + y
+
+
+
+
