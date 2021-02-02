c52eca6bc7b08476b958c9baf6fe416a9c484d24
Denis Naughton <mjolinor75@hotmail.com>
2021-02-02 15:16:02 -0500

    Added array to arrow.js

---
+++ javascript/array_methods/arrow_methosd/arrow.js
@@ -0,0 +1,22 @@
+const movies = [
+    {
+      
+      title: 'Amadeus',
+      score: 99
+  
+    },
+    {
+  
+    title: 'Stand By Me',
+    score: 85
+  
+    },
+  
+    {
+        title: 'Parasite',
+        score: 95
+    },
+  
+    {
+        title: 'Alien',
+        score: 90
