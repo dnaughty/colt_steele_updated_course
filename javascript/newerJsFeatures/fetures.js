f9ab95897e2b0b4f12a349db16e24fcb65bd39ae
Denis Naughton <mjolinor75@hotmail.com>
2021-02-05 16:14:17 -0500

    Spread array

--- javascript/newerJsFeatures/fetures.js
+++ javascript/newerJsFeatures/fetures.js
@@ -1,3 +1,9 @@
-function rollDie(numSides = 6) {
-    return Math.floor(Math.random() * numSides) + 1
-}
+// function rollDie(numSides = 6) {
+//     return Math.floor(Math.random() * numSides) + 1
+// }
+
+
+const people = ["Denis", "Naomi", "Tim", "Mike"]
+const others = ["Veronica", "Steph"]
+
+console.log([...people, ...others])
